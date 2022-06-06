b9ff16052adb9dad5e83e9fdee2c308135f62e57
Lukas Engel <79928782+Respicite@users.noreply.github.com>
2022-06-06 22:35:34 +0200

    Added create_todo

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+class ToDo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="Priority must be between 1 and 5")
+    complete: bool
 
 
 def get_db():
@@ -26,9 +35,25 @@ async def read_all(db: Session = Depends(get_db)):
 
 
 @app.get("/todo/{id}")
-async def read_all(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
-    if todo is not None:
-        return todo
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
     item_not_found()
-    
+
+
+@app.post("/")
+async def create_todo(todo: ToDo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 201,
+        'transaction': 'successful'
+    }
