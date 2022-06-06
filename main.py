20cb50e60d6a356e1b3a1d74c6e5b05265597a28
Lukas Engel <79928782+Respicite@users.noreply.github.com>
2022-06-06 22:24:36 +0200

    Added get by id

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close
+
+
+def item_not_found():
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
 @app.get("/")
-async def create_database():
-    return {'Database': 'Online'}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
+
+
+@app.get("/todo/{id}")
+async def read_all(todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo is not None:
+        return todo
+    item_not_found()
+    
