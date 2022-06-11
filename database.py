2dfbabdf758bd8bdc71fb3a658a77a31cddb0ae1
Lukas Engel <79928782+Respicite@users.noreply.github.com>
2022-06-11 10:26:17 +0200

    SQLite -> PostgreSQL

--- database.py
+++ database.py
@@ -2,11 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
-
-engine = create_engine(
-    SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5433/postgres'
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,
                             autoflush=False,
