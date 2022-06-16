40f2042054524f34e937dfb1eeb05acfbc2a0a5f
Lukas Engel <79928782+Respicite@users.noreply.github.com>
2022-06-16 23:07:53 +0200

    Changed SQL_ALCHEMY_DATABASE_URL

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQL_ALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
-if SQL_ALCHEMY_DATABASE_URL.startswith('postgres://'):
-    SQL_ALCHEMY_DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+# if SQL_ALCHEMY_DATABASE_URL.startswith('postgres://'):
+#    SQL_ALCHEMY_DATABASE_URL.replace('postgres://', 'postgresql://', 1)
 # SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5433/postgres'
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
