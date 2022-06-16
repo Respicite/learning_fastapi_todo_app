64619caf52cbd3827a843f04b57f904530fcd5b5
Lukas Engel <79928782+Respicite@users.noreply.github.com>
2022-06-16 21:52:55 +0200

    Added production postgres db

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5433/postgres'
+SQL_ALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQL_ALCHEMY_DATABASE_URL.startswith('postgres://'):
+    SQL_ALCHEMY_DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+# SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost:5433/postgres'
+
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,
