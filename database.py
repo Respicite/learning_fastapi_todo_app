08af7c82d32267ec753aff214d858f8095753ec9
Lukas Engel <79928782+Respicite@users.noreply.github.com>
2022-06-16 23:35:10 +0200

    Removed some debug code

--- database.py
+++ database.py
@@ -3,15 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-db_string = os.environ.get('DATABASE_URL')
+db_string = os.environ.get('DATABASE_URL_PG')
 print(db_string)
-print("=====")
-if db_string.startswith("postgres://"):
-    db_string.replace("postgres://", "postgresql://")
-    print(db_string)
 
 engine = create_engine(db_string)
-print("=====")
 
 SessionLocal = sessionmaker(autocommit=False,
                             autoflush=False,
